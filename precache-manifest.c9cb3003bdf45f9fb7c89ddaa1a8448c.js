c7c0894de232fec7b22e8e5372b9cdf75a7c824f
Guanine-19 <fudgeebarr12@gmail.com>
2020-04-25 14:31:01 +0800

    Updates

---
+++ precache-manifest.c9cb3003bdf45f9fb7c89ddaa1a8448c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2769510218b17a6de7844c462fbe92",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "5ab8f82764b0de5b2078",
+    "url": "/Pomodoro-Clock/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "3455d333ea2ffdb8eac4",
+    "url": "/Pomodoro-Clock/static/css/main.8478c3f7.chunk.css"
+  },
+  {
+    "revision": "5ab8f82764b0de5b2078",
+    "url": "/Pomodoro-Clock/static/js/2.546cea7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.546cea7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3455d333ea2ffdb8eac4",
+    "url": "/Pomodoro-Clock/static/js/main.12ed96ce.chunk.js"
+  },
+  {
+    "revision": "c3a62dc02c97209aa3a4",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.14da358f.js"
+  }
+]);
